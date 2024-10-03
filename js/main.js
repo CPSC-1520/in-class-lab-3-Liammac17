7a133f63588d855708659843926e9bd3d5affc0a
Liam Macumber <liammacumber15@gmail.com>
2024-10-02 22:37:49 -0600

    done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-// Enter your code below.
-//test
-console.log('test');
+
+const resources = document.querySelector(".javascript-resources")
+const button = document.querySelector("show-resources")
+const show = resources.querySelectorAll("a")
+
+button.addEventListener("click", function(e){
+    resources.classList.remove("d-none")
+})
+
+show.addEventListener("mouseover", function(e){
+    console.log(e.target)
+    e.target.classList.add("fw-bold")
+})
+
+show.addEventListener("mouseout", function(e){
+    e.target.classList.remove("fw-bold")
+})
+
+resources.addEventListener("click", function(e){
+    e.target.classList.add("fst-italic")
+})
